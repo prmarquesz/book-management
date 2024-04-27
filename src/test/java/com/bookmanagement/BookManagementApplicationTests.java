7bcaba363490bab3e08411a5a05a2c9916bc5025
Paulo <paulormarques21@gmail.com>
2024-04-27 12:21:48 -0300

    infra: setup spring boot

---
+++ src/test/java/com/bookmanagement/BookManagementApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bookmanagement;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+        Assertions.assertTrue(true);
+    }
+
+}
